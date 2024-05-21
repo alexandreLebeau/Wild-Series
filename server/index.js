0364b2ea8837951691bf71aa1fe00d2b860f5cad
ALEXANDRE LEBEAU <alexandre.lebeau4@gmail.com>
2024-05-21 10:36:03 +0200

    quete 2

--- server/index.js
+++ server/index.js
@@ -9,8 +9,18 @@ require("./database/client").checkConnection();
 // Import the Express application from app/config.js
 const app = require("./app/config");
 
+// Add a root GET route
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page!");
+});
+
+// Add a new GET route
+app.get("/new-route", (req, res) => {
+  res.send("Hello from the new route!");
+});
+
 // Get the port from the environment variables
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || 3300; // Ensure the server listens on port 3310
 
 // Start the server and listen on the specified port
 app
