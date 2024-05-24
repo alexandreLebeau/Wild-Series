e9dc8653189c70e2b623df5ce2e53ae0ff9e2e3f
ALEXANDRE LEBEAU <alexandre.lebeau4@gmail.com>
2024-05-24 10:26:29 +0200

    quete 4

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,13 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const { sayWelcome } = require("../../controllers/sayActions");
-
-router.get("/", sayWelcome);
-
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
 
 const itemsRouter = require("./items/router");
 
@@ -17,6 +16,10 @@ router.use("/items", itemsRouter);
 const programsRouter = require("./programs/router");
 
 router.use("/programs", programsRouter);
+
+const categoriesRouter = require("./categories/router");
+
+router.use("/categories", categoriesRouter);
 
 /* ************************************************************************* */
 
